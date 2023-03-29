2f6938d8e43572d83b7a73b7bcc1ff55c1c4d921
Titilope Ajayi oluwaseyi <ajayititilope357@gmail.com>
2023-03-30 00:31:08 +0100

    min sdk changed home screen design

--- app/src/main/java/com/example/ttwwapp/HomeActivity.kt
+++ app/src/main/java/com/example/ttwwapp/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.example.ttwwapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ttwwapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var homeBinding:ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        homeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = homeBinding.root
         setContentView(R.layout.activity_home)
     }
 }
