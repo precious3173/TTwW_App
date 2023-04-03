2cd8728e542ce102d7c384b37bffb4138bd4d00e
Titilope Ajayi oluwaseyi <88669602+Titilegend@users.noreply.github.com>
2023-04-03 18:16:00 +0100

    Second branch (#2)
    * compile sdk changed to 33
    viewBinding used
    Splash screen
    
    * min sdk changed
    home screen design
    
    * text alignment
    
    ---------
    
    Co-authored-by: Precious Peter <66275437+precious3173@users.noreply.github.com>

--- app/src/main/java/com/example/ttwwapp/HomeActivity.kt
+++ app/src/main/java/com/example/ttwwapp/HomeActivity.kt
@@ -2,10 +2,14 @@ package com.example.ttwwapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ttwwapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var homeBinding:ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-    }
+        homeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = homeBinding.root
+}
 }
