323264a1a13c0647f916f4c9a713300cabbcd12d
Admin <preciouspeter3173@gmail.com>
2023-04-09 15:03:39 +0100

    Recyclerview Adapter created

---
+++ app/src/main/java/com/example/ttwwapp/Adapter/RecyclerAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.ttwwapp.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter(): RecyclerView.Adapter<RecyclerAdapter.Answer>() {
+
+    class Answer : RecyclerView.ViewHolder(){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Answer {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Answer, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
